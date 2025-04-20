b2b67d50c34d4baae5606558aed77a330f0c6816
AlifaSrn <alifa.nub@gmail.com>
2025-04-20 20:13:23 +0600

    first commit

---
+++ Js/script.js
@@ -0,0 +1,56 @@
+document.getElementById('menuToggle').addEventListener('click', ()=>{
+    document.getElementById('navbar').classList.toggle('show');
+});
+
+// Switch main product image on thumbnail click
+document.querySelectorAll('.thumb').forEach(thumb =>{
+    thumb.addEventListener('click', ()=>{
+        document.getElementById('mainImage').src = thumb.src;
+    });
+});
+
+const slider = document.getElementById("slider");
+const slides = document.querySelectorAll(".slide");
+const dotContainer = document.getElementById("dots");
+
+//current slide index
+let currentIndex = 0;
+
+//slide control function
+slides.forEach((_, index) =>{
+    const dot = document.createElement("span");
+    dot.classList.add("dot");
+    dot.addEventListener("click", () => goToSlide(index));
+    dotContainer.appendChild(dot);
+});
+
+const goToSlide = (index) => {
+    currentIndex = index;
+    slider.style.transform = `translateX(-${index * 100}%)`;
+    updateDots();
+}
+
+
+const updateDots = () => {
+    const allDots = dotContainer.querySelectorAll("span");
+    allDots.forEach((dot, idx)=>{
+        dot.classList.toggle("active", idx === currentIndex);
+    });
+};
+
+//prev Button + event
+const prevBtn = document.querySelector(".nav-btn:not(.next)");
+prevBtn.addEventListener("click", () =>{
+        const newIndex = (currentIndex - 1 + slides.length) % slides.length;
+        goToSlide(newIndex);
+});
+
+//Next Button + event
+const nextBtn = document.querySelector(".nav-btn.next");
+nextBtn.addEventListener("click", () =>{
+        const newIndex = (currentIndex + 1) % slides.length;
+        goToSlide(newIndex);
+});
+
+goToSlide(0);
+
